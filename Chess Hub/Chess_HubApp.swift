82839b8cbe0ab6a8f6f4d72b37ec2fff27e8e636
Sebastian Strus <sebastian.strus1@gmail.com>
2026-03-14 16:29:04 +0100

    Initial Commit

---
+++ Chess Hub/Chess_HubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chess_HubApp.swift
+//  Chess Hub
+//
+//  Created by Sebastian Strus on 3/14/26.
+//
+
+import SwiftUI
+
+@main
+struct Chess_HubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
